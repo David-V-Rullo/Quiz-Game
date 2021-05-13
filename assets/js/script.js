84baaf7b5f44047bc19803614bba13136736052a
David-V-Rullo <d.v.rullo@gmail.com>
2021-05-13 06:40:05 -0400

    A few before bed changes, nothing substantial. Still not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,27 +44,47 @@ var questions = [
     correctAnswer: "answer-D",
   }),
 ];
+var timer;
+var timerCount;
 var answerA = $("#answer-A");
 var answerB = $("#answer-B");
 var answerC = $("#answer-C");
 var answerD = $("#answer-D");
 var questionText = $("#question-text");
-var submitButton = $(".btn btn-primary");
+var submitButton = $("#submit-button");
+var startButton = $("#start-button");
+var timerText = $("#timer-counter")
+//Variables for storing correct and incorrect answers; to be passed to high score and local storage. 
 var correctAnswer = 0;
 var wrongAnswer = 0;
 var chosenQuestion = {};
 //Use a function to randomly choose the next question
 //Function to render the question in the box.
-
+function startTimer() {
+    timer = setInterval(function(){
+        timerCount--;
+        timerText.text(timerCount);
+        if (timerCount === 0) {
+            return 
+        }
+    }, 1000)
+}
 function pullQuestion(arr) {
   var random = Math.floor(Math.random() * arr.length);
   var randQuestion = arr[random];
 
   return randQuestion;
 }
-
+function startGame() {
+    console.log("It started")
+    timerCount = 15;
+    renderQuestion()
+    startTimer()
+}
 function renderQuestion() {
+    //Uses random number to pull a question
   chosenQuestion = pullQuestion(questions);
+  //Populate the various fields needed to show the question and possible answers to user
   questionText.text(chosenQuestion.question);
   answerA.next().text(chosenQuestion.optA);
   answerB.next().text(chosenQuestion.optB);
@@ -87,7 +107,7 @@ function renderQuestion() {
 // Check the input and compare against the correct answer key in obj(questions)
 function handleAnswerSubmit(event) {
   event.preventDefault();
-
+    timerCount = 15
   var submittedAnswer = $("input:checked");
   console.log(submittedAnswer);
   if (submittedAnswer === chosenQuestion.correctAnswer) {
@@ -99,5 +119,5 @@ function handleAnswerSubmit(event) {
   localStorage.setItem("Wrong", wrongAnswer);
 }
 
-renderQuestion();
+startButton.on("click", startGame);
 submitButton.on("submit", handleAnswerSubmit);
