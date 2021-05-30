1f07d6a1c58934044bc10887af729539624396cf
David-V-Rullo <d.v.rullo@gmail.com>
2021-05-30 13:33:05 -0400

    Coming along again. Complete rewrite of the question objects and heavy editing of the HTML and CSS

---
+++ assets/Assets/js/questions.js
@@ -0,0 +1,47 @@
+var questions = [
+  {
+    questionOne:
+      "What amendment to the Constitution protects the freedom of speech?",
+    answers: [
+      { text: "14th", correct: false },
+      { text: "2nd", correct: false },
+      { text: "5th", correct: false },
+      { text: "1st", correct: true },
+    ],
+  },
+  {questionTwo:
+      "What element of a crime reflects an individual's state of mind?",
+    answers: [
+      { text: "Res Judicata", correct: false },
+      { text: "Mens Rea", correct: true },
+      { text: "Proximate Cause", correct: false },
+      { text: "Fault", correct: false },
+    ],
+  },
+  {questionThree:
+      "Which of the following is not required to form a contract under the UCC",
+    answers: [
+      { text: "A price", correct: true },
+      { text: "Acceptance", correct: false },
+      { text: "Consideration", correct: false },
+      { text: "An Offer", correct: false },
+    ]
+},
+  {questionFour:
+      "What year did the U.S. ratify it's current Constitution?",
+    answers: [
+      { text: "1789", correct: true },
+      { text: "1776", correct: false },
+      { text: "1985", correct: false },
+      { text: "1781", correct: false },
+    ]
+},
+  {questionFive:
+    "What amendment to the Constitution grants the right to a jury trial",
+    answers: [
+      { text: "5th", correct: false },
+      { text: "8th", correct: false },
+      { text: "14th", correct: false },
+      { text: "6th", correct: true },
+    ]
+  }]
--- assets/Assets/js/script.js
+++ assets/Assets/js/script.js
@@ -1,49 +1,5 @@
-var rootEl = $("#root")
-var questions = [
-  (questionOne = {
-    question:
-      "What amendment to the Constitution protects the freedom of speech?",
-    optA: "14th",
-    optB: "2nd",
-    optC: "5th",
-    optD: "1st",
-    correctAnswer: "answer-D",
-  }),
-  (questionTwo = {
-    question: "What element of a crime reflects an individual's state of mind?",
-    optA: "Res Judicata",
-    optB: "Mens Rea",
-    optC: "Proximate Cause",
-    optD: "Fault",
-    correctAnswer: "answer-B",
-  }),
-  (questionThree = {
-    question:
-      "Which of the following is not required to form a contract under the UCC?",
-    optA: "A price",
-    optB: "Acceptance",
-    optC: "Consideration",
-    optD: "An Offer",
-    correctAnswer: "answer-A",
-  }),
-  (questionFour = {
-    question: "What year did the U.S. ratify it's current Constitution?",
-    optA: "1789",
-    optB: "1776",
-    optC: "1985",
-    optD: "1781",
-    correctAnswer: "answer-A",
-  }),
-  (questionFive = {
-    question:
-      "What amendment to the Constitution grants the right to a jury trial",
-    optA: "5th",
-    optB: "8th",
-    optC: "14th",
-    optD: "6th",
-    correctAnswer: "answer-D",
-  }),
-];
+var rootEl = $("#root");
+
 var timer;
 var timerCount;
 var answerA = $("#answer-A");
@@ -53,21 +9,21 @@ var answerD = $("#answer-D");
 var questionText = $("#question-text");
 var submitButton = $("#submit-button");
 var startButton = $("#start-button");
-var timerText = $("#timer-counter")
-//Variables for storing correct and incorrect answers; to be passed to high score and local storage. 
+var timerText = $("#timer-counter");
+//Variables for storing correct and incorrect answers; to be passed to high score and local storage.
 var correctAnswer = 0;
 var wrongAnswer = 0;
 var chosenQuestion = {};
 //Use a function to randomly choose the next question
 //Function to render the question in the box.
 function startTimer() {
-    timer = setInterval(function(){
-        timerCount--;
-        timerText.text(timerCount);
-        if (timerCount === 0) {
-            return 
-        }
-    }, 1000)
+  timer = setInterval(function () {
+    timerCount--;
+    timerText.text(timerCount);
+    if (timerCount === 0) {
+      return;
+    }
+  }, 1000);
 }
 function pullQuestion(arr) {
   var random = Math.floor(Math.random() * arr.length);
@@ -76,13 +32,13 @@ function pullQuestion(arr) {
   return randQuestion;
 }
 function startGame() {
-    console.log("It started")
-    timerCount = 15;
-    renderQuestion()
-    startTimer()
+  console.log("It started");
+  timerCount = 15;
+  renderQuestion();
+  startTimer();
 }
 function renderQuestion() {
-    //Uses random number to pull a question
+  //Uses random number to pull a question
   chosenQuestion = pullQuestion(questions);
   //Populate the various fields needed to show the question and possible answers to user
   questionText.text(chosenQuestion.question);
@@ -107,7 +63,7 @@ function renderQuestion() {
 // Check the input and compare against the correct answer key in obj(questions)
 function handleAnswerSubmit(event) {
   event.preventDefault();
-    timerCount = 15
+  timerCount = 15;
   var submittedAnswer = $("input:checked");
   console.log(submittedAnswer);
   if (submittedAnswer === chosenQuestion.correctAnswer) {
